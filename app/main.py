cb6ef49798b32cb2fb5d34288eabbd6b99c257a4
Tom <120240787+QuangPhamvt@users.noreply.github.com>
2024-02-22 09:39:07 +0700

    chore: devops

--- app/main.py
+++ app/main.py
@@ -13,3 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/users/me")
+def read_user_me():
+    return {"user_id": "the current user"}
