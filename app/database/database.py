dc3a8f93c8ee5741a730c9943613bc9e18425066
Tom <120240787+QuangPhamvt@users.noreply.github.com>
2024-02-25 22:33:06 +0700

    feat: #12 fix devops and structure folder

--- app/database/database.py
+++ app/database/database.py
@@ -1,4 +1,3 @@
-import os
 from sqlalchemy import URL
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncAttrs
@@ -6,14 +5,13 @@ from dotenv import dotenv_values
 from app.configs.constants import DB_PORT
 
 config = dotenv_values(".env")
-HOST = os.getenv("DB_HOST")
 
 # check type in python is number
 url_create = URL.create(
     drivername="mysql+asyncmy",
     username=config["DB_USERNAME"],
     password=config["DB_PASSWORD"],
-    host=HOST,
+    host=config["DB_HOST"],
     port=DB_PORT,
     database=config["DB_NAME"],
 )
