ea094c64fd2359c99b50dd5c82b0a87ca743f339
nqmihn <qminhqb@gmail.com>
2024-03-17 01:35:35 +0700

    fix: 34 delete service foler in route

--- app/routers/user/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .get_list_users import GetList
-from .create_demo_user import CreateDemoUser
-
-
-class UserService(GetList, CreateDemoUser):
-    pass
-
-
-__all__ = ["UserService"]
--- app/routers/user/services/get_list_users.py
+++
@@ -1,8 +0,0 @@
-from app.database.crud import user_crud
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-class GetList:
-    async def get_list_users(self, db: AsyncSession):
-        list_users = await user_crud.get_list_users(db)
-        return list_users
