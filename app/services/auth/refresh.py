e619ccefd590f72b325df2ebb697818056750a7f
Tom <120240787+QuangPhamvt@users.noreply.github.com>
2024-03-26 19:21:53 +0700

    fix: #59 fix bug at and rt

--- app/services/auth/refresh.py
+++ app/services/auth/refresh.py
@@ -4,27 +4,38 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.crud import user_crud
 from app.schemas.auth import TokenPayload
 from app.utils.helper import helper
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 
 class Refresh:
     async def refresh(self, request: Request, response: Response, db: AsyncSession):
         payload = await self.__is_valid_token(request=request, db=db)
         if payload:
+            at_seconds = 600
+            rt_seconds = 604800
+
+            if config["ACCESS_TOKEN_EXPIRE"]:
+                at_seconds = int(config["ACCESS_TOKEN_EXPIRE"])
+            if config["REFRESH_TOKEN_EXPIRE"]:
+                rt_seconds = int(config["REFRESH_TOKEN_EXPIRE"])
             new_access_token = helper.create_access_token(user=payload)
             new_refresh_token = helper.create_refresh_token(user=payload)
+
             response.set_cookie(
                 "access_token",
                 new_access_token,
+                at_seconds,
                 secure=True,
-                max_age=99999999,
-                expires=99999999,
+                httponly=True,
             )
             response.set_cookie(
                 "refresh_token",
                 new_refresh_token,
+                rt_seconds,
                 secure=True,
-                max_age=99999999,
-                expires=99999999,
+                httponly=True,
             )
             user_id = payload.id
             await user_crud.update_refresh_token(
